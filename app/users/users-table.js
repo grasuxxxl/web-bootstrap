eed69df0d573e3e873bee01226382e35adef7f63
Maximilian Babescu <maximilian1991@gmail.com>
2015-06-07 15:00:09 +0300

    Added FixedDataTable as display component for users.

--- app/users/users-table.js
+++ app/users/users-table.js
@@ -2,28 +2,28 @@
  * Created by Maximilian on 5/30/2015.
  */
 import React, { Component, PropTypes } from 'react';
+import { Table, Column } from 'fixed-data-table';
 
 export default class UsersTable extends Component {
 
-    render () {
-        var listOfUsers = this.makeListOfUsers(this.props.users);
-
-        return (
-            <div>
-                {listOfUsers}
-            </div>
-        );
+    _rowGetter (index) {
+        return this.props.users[index];
     }
 
-    makeListOfUsers (users) {
-        return users.map(function (user) {
-            var name = user.firstName + ' ' + user.lastName;
-            return (
-                <div>
-                    <span>{name}</span>
-                </div>
-            );
-        });
+    render () {
+
+        return (
+            <Table
+                width={200}
+                height={600}
+                rowsCount={this.props.users.length}
+                rowHeight={50}
+                rowGetter={this._rowGetter.bind(this)}
+                headerHeight={50}>
+                <Column label="FirstName" width={100} dataKey={'firstName'} />
+                <Column label="LastName" width={100} dataKey={'lastName'}/>
+            </Table>
+        );
     }
 }
 
--- app/users/users.js
+++ app/users/users.js
@@ -17,7 +17,6 @@ export default class Users extends Component {
     }
 
     render () {
-        debugger;
         return (
             <div>
                 <UsersTable users={this.state.users} />
